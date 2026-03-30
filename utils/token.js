d213f932b18346420c5f36831144e507addd5b1d
E. Pierre <7chrispeter@gmail.com>
2026-03-30 00:27:57 -0400

    address jwt secret error

--- utils/token.js
+++ utils/token.js
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken';
 
+if (!process.env.JWT_SECRET) {
+  throw new Error('Missing required env var: JWT_SECRET');
+}
 export const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
 };
